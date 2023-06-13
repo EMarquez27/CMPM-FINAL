e319f64fc6388fb9404860ed4c0b145c48ce237b
EMarquez27 <105992805+EMarquez27@users.noreply.github.com>
2023-06-12 20:50:22 -0700

    script

--- src/script.js
+++ src/script.js
@@ -4,6 +4,64 @@ class Title extends Phaser.Scene{
     }
 
     preload() {
-        this.preload.path
+        
+    }
+
+    create() {
+
     }
 }
+
+class Settings extends Phaser.Scene {
+    constructor() {
+        super('settings')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Outro extends Phaser.Scene {
+    constructor() {
+        super('outro')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+const game = new Phaser.Game({
+    scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1900,
+    height: 1000
+    },
+
+    type: Phaser.AUTO,
+    scene: [Title, Settings, Intro, Outro]
+})
