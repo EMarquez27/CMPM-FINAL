4185f4889e5463cf18e015eaa5ab420eb792f0bf
EMarquez27 <105992805+EMarquez27@users.noreply.github.com>
2023-06-12 21:06:08 -0700

    changed script

--- src/script.js
+++ src/script.js
@@ -40,6 +40,160 @@ class Intro extends Phaser.Scene {
     }
 }
 
+class Rae1 extends Phaser.Scene {
+    constructor() {
+        super('rae1')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Rae2 extends Phaser.Scene {
+    constructor() {
+        super('rae2')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Violet1 extends Phaser.Scene {
+    constructor() {
+        super('violet1')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Violet2 extends Phaser.Scene {
+    constructor() {
+        super('violet2')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Violet3 extends Phaser.Scene {
+    constructor() {
+        super('violet3')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Violet4 extends Phaser.Scene {
+    constructor() {
+        super('violet4')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Nero1 extends Phaser.Scene {
+    constructor() {
+        super('nero1')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Nero2 extends Phaser.Scene {
+    constructor() {
+        super('nero2')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Nero3 extends Phaser.Scene {
+    constructor() {
+        super('nero3')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Nero4 extends Phaser.Scene {
+    constructor() {
+        super('nero4')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro')
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+
+    }
+}
+
 class Outro extends Phaser.Scene {
     constructor() {
         super('outro')
