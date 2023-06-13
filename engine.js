7e19504450aa8e4b1345cf37091087e93a581c7b
EMarquez27 <105992805+EMarquez27@users.noreply.github.com>
2023-06-12 20:58:29 -0700

    added stuff

---
+++ engine.js
@@ -0,0 +1,66 @@
+class Engine {
+
+    static load(...args) {
+        window.onload = () => new Engine(...args);
+    }
+
+    constructor(firstSceneClass, storyDataUrl) {
+
+        this.firstSceneClass = firstSceneClass;
+        this.storyDataUrl = storyDataUrl;
+
+        this.header = document.body.appendChild(document.createElement("h1"));
+        this.output = document.body.appendChild(document.createElement("div"));
+        this.actionsContainer = document.body.appendChild(document.createElement("div"));
+
+        fetch(storyDataUrl).then(
+            (response) => response.json()
+        ).then(
+            (json) => {
+                this.storyData = json;
+                this.gotoScene(firstSceneClass)
+            }
+        );
+    }
+
+    gotoScene(sceneClass, data) {
+        this.scene = new sceneClass(this);
+        this.scene.create(data);
+    }
+
+    addChoice(action, data) {
+        let button = this.actionsContainer.appendChild(document.createElement("button"));
+        button.innerText = action;
+        button.onclick = () => {
+            while(this.actionsContainer.firstChild) {
+                this.actionsContainer.removeChild(this.actionsContainer.firstChild)
+            }
+            this.scene.handleChoice(data);
+        }
+    }
+
+    setTitle(title) {
+        document.title = title;
+        this.header.innerText = title;
+    }
+
+    show(msg) {
+        let div = document.createElement("div");
+        div.innerHTML = msg;
+        this.output.appendChild(div);
+    }
+}
+
+class Scene {
+    constructor(engine) {
+        this.engine = engine;
+    }
+
+    create() { }
+
+    update() { }
+
+    handleChoice(action) {
+        console.warn('no choice handler on scene ', this);
+    }
+}
