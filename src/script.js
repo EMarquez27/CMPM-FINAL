df00de459da434216a9c6619cbde9ca030286df3
EMarquez27 <105992805+EMarquez27@users.noreply.github.com>
2023-06-14 17:33:07 -0700

    changed some stuff

--- src/script.js
+++ src/script.js
@@ -9,15 +9,27 @@ class Studio extends Phaser.Scene
         this.load.path = './assets/';
         this.load.image('studio', 'studio.png');
         this.load.audio('bgm', 'bgm.mp3');
-       // this.load.spritesheet("gif", "gif4.png", {frameWidth: 1200, frameHeight: 1080});
+        //this.load.spritesheet("gif", "./assets/gif4.png", {frameWidth: 1200, frameHeight: 1080});
+        this.load.spritesheet("gif", "./assets/TatStudio.gif", {frameWidth: 1200, frameHeight: 1080});
         //this.load.audio('Boom', 'boom.mp3');
     }
 
     create ()
     {  
         this.cameras.main.fadeIn(1000, 0,0,0);
-        let sound = this.sound.add('bgm');
+        let sound = this.sound.add('bgm', {loop: true});
         sound.play();
+
+        this.load.spritesheet({
+            key: "gif",
+            url: "./assets/TatStudio.gif",
+            frameConfig: {
+                frameWidth: 1200,
+                frameHeight: 1080,
+                startFrame: 0,
+                endFrame: 28
+            }
+        });
         //let sound2 = this.sound.add('Boom');
         //this.time.delayedCall(1000, () => {
         //    sound2.play();
@@ -113,7 +125,7 @@ class MainMenu extends Phaser.Scene
             'start',//imagename
         )
         start.setInteractive()
-        start.on('pointerdown', () => this.scene.start('gameplay'));
+        start.on('pointerdown', () => this.scene.start('intro'));
 
         let credit = this.add.image(
             960,//x
@@ -172,16 +184,22 @@ class Options extends Phaser.Scene
 
     create ()
     {
-        let options = this.imageObject = this.add.image(
-            960,//x
+        let options = this.add.text(
+            810,//x
             500,
-            'menuopt',//imagename
+            "Turn OFF Sound",
         )
+        .setScale(2)
+        
+        options.setInteractive()
+        options.on('pointerdown', () => sound.pause());
+        
         let back = this.imageObject = this.add.image(
-            960,
-            500,
+            200,
+            200,
             'back',
         )
+        back.setScale(0.2)
         back.setInteractive()
         back.on('pointerdown', () => this.scene.start('menu'));
     }
@@ -210,45 +228,17 @@ class Credits extends Phaser.Scene
             'credit',//imagename
         )
         let back = this.imageObject = this.add.image(
-            900,
-            400,
+            200,
+            200,
             'back',
         )
+        options.setScale(0.6)
+        back.setScale(0.2)
         back.setInteractive()
         back.on('pointerdown', () => this.scene.start('menu'));
     }
 }
 
-
-class Title extends Phaser.Scene{
-    constructor() {
-        super('title')
-    }
-
-    preload() {
-        this.load.path = './assets/'; 
-    }
-
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
-    }
-}
-
-class Settings extends Phaser.Scene {
-    constructor() {
-        super('settings')
-    }
-
-    preload() {
-        this.load.path = './assets/';
-    }
-
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
-    }
-}
 
 class Intro extends Phaser.Scene {
     constructor() {
@@ -256,16 +246,20 @@ class Intro extends Phaser.Scene {
     }
 
     preload() {
-        this.load.path = './assets/';
+        this.load.path = './assets/'; 
     }
 
     create() {
+        this.add.text(game.config.width/2, game.config.height/2, "Freya Amara was a distinguished veteran of war, \ncelebrated for her valor and combat abilities, \nshe was chosen to be the first test subject for an advanced cybernetic project.\n\n\n The EIGEN project")
+            .setFontSize(35)
+            .setOrigin(0.5)
         .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+        .on('pointerdown', () => this.scene.start('Freya1'));
     }
 }
 
-class Freya1 extends Phaser.Scene {
+
+class Freya1 extends AdventureScene {
     constructor() {
         super('Freya1')
     }
@@ -275,13 +269,14 @@ class Freya1 extends Phaser.Scene {
         this.load.image('freya', 'Freya.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
+        //.setInteractive()
+        //.on('pointerdown', () => this.scene.start('menu'));
     }
 }
 
-class Freya2 extends Phaser.Scene {
+class Freya2 extends AdventureScene {
     constructor() {
         super('Freya2')
     }
@@ -291,13 +286,14 @@ class Freya2 extends Phaser.Scene {
         this.load.image('freya', 'Freya.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
+        //.setInteractive()
+        //.on('pointerdown', () => this.scene.start('menu'));
     }
 }
 
-class Violet1 extends Phaser.Scene {
+class Violet1 extends AdventureScene {
     constructor() {
         super('violet1')
     }
@@ -308,14 +304,12 @@ class Violet1 extends Phaser.Scene {
         this.load.image('violet', 'Violet.png');
     }
 
-    create() {
-        this.load.text("")
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Violet2 extends Phaser.Scene {
+class Violet2 extends AdventureScene {
     constructor() {
         super('violet2')
     }
@@ -326,13 +320,12 @@ class Violet2 extends Phaser.Scene {
         this.load.image('violet', 'Violet.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Violet3 extends Phaser.Scene {
+class Violet3 extends AdventureScene {
     constructor() {
         super('violet3')
     }
@@ -343,14 +336,13 @@ class Violet3 extends Phaser.Scene {
         this.load.image('violet', 'Violet.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
         
     }
 }
 
-class Violet4 extends Phaser.Scene {
+class Violet4 extends AdventureScene {
     constructor() {
         super('violet4')
     }
@@ -361,13 +353,12 @@ class Violet4 extends Phaser.Scene {
         this.load.image('violet', 'Violet.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Bianca1 extends Phaser.Scene {
+class Bianca1 extends AdventureScene {
     constructor() {
         super('Bianca1')
     }
@@ -378,13 +369,12 @@ class Bianca1 extends Phaser.Scene {
         this.load.image('bianca', 'Bianca.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Bianca2 extends Phaser.Scene {
+class Bianca2 extends AdventureScene {
     constructor() {
         super('Bianca2')
     }
@@ -395,13 +385,12 @@ class Bianca2 extends Phaser.Scene {
         this.load.image('bianca', 'Bianca.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Bianca3 extends Phaser.Scene {
+class Bianca3 extends AdventureScene {
     constructor() {
         super('Bianca3')
     }
@@ -412,13 +401,12 @@ class Bianca3 extends Phaser.Scene {
         this.load.image('bianca', 'Bianca.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Bianca4 extends Phaser.Scene {
+class Bianca4 extends AdventureScene {
     constructor() {
         super('Bianca4')
     }
@@ -429,29 +417,12 @@ class Bianca4 extends Phaser.Scene {
         this.load.image('bianca', 'Bianca.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
-class Intro extends Phaser.Scene {
-    constructor() {
-        super('intro')
-    }
-
-    preload() {
-        this.load.path = './assets/';
-        this.load.image('freya', 'Freya.png');
-    }
-
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
-    }
-}
-
-class Outro extends Phaser.Scene {
+class Outro extends AdventureScene {
     constructor() {
         super('outro')
     }
@@ -461,9 +432,8 @@ class Outro extends Phaser.Scene {
         this.load.image('freya', 'Freya.png');
     }
 
-    create() {
-        .setInteractive()
-        .on('pointerdown', () => this.scene.start('menu'));
+    onEnter() {
+        this.load.image('freya');
     }
 }
 
@@ -476,6 +446,6 @@ const game = new Phaser.Game({
     },
 
     type: Phaser.AUTO,
-    //scene: [Studio, MainMenu, Options, Title, Settings, Intro, Outro]
-    scene: [Studio],
+    //scene: [Studio, MainMenu, Credits, Options, Intro, Freya1],
+    scene: [Freya1],
 });
